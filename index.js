82205817cdc3d406ba1da8a2f851e752f3e1b648
Dave Boehmer <daboehmer@gmail.com>
2019-04-08 03:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ function updateObjectWithKeyAndValue(object, key, value) {
   Object.assign({}, object, {key: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
-  return object;
-}
